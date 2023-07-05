38a74be959e99a95d3ce1554176fdef5aa79da0e
Emine Tekcan <eminetek68can@gmail.com>
2023-07-05 23:20:02 +0300

    Product pagination completed

--- src/components/home/Products.jsx
+++ src/components/home/Products.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getProducts } from '../../redux/features/productSlice';
 import { STATUS } from '../../utils/status';
 import Loading from '../Loading';
 import Product from './Product';
+import ReactPaginate from 'react-paginate';
 
 const Products = () => {
 
@@ -16,6 +17,20 @@ const Products = () => {
     dispatch(getProducts())
   }, [dispatch])
 
+  const [itemOffset, setItemOffset] = useState(0);
+  const itemsPerPage = 6;
+
+  const endOffset = itemOffset + itemsPerPage;
+  const currentItems = products.slice(itemOffset, endOffset);
+  const pageCount = Math.ceil(products.length / itemsPerPage);
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % products.length;
+    console.log(
+      `User requested page number ${event.selected}, which is offset ${newOffset}`
+    );
+    setItemOffset(newOffset);
+  };
 
   return (
     <div className='m-auto'>
@@ -23,9 +38,21 @@ const Products = () => {
         productStatus == STATUS.LOADING ?
           <Loading /> :
 
-          <div  className='flex flex-wrap'>
-           { products.map((item, index) => <Product key={index} data={item} />)}
-          </div>
+          <>
+            <div className='flex flex-wrap'>
+              {currentItems.map((item, index) => <Product key={index} data={item} />)}
+            </div>
+            <ReactPaginate
+              className="paginate"
+              breakLabel="..."
+              nextLabel="next >"
+              onPageChange={handlePageClick}
+              pageRangeDisplayed={5}
+              pageCount={pageCount}
+              previousLabel="< previous"
+              renderOnZeroPageCount={null}
+            />
+          </>
       }
     </div>
   )
