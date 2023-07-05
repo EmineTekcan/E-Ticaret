a84b3402a61606903fff4f9561d8768e8230e43b
Emine Tekcan <eminetek68can@gmail.com>
2023-07-05 17:51:40 +0300

    Project started and Navbar created

---
+++ src/components/navbar/navbarItem/NavbarRight.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NavbarRight = () => {
+    return (
+        <div className=''>
+            <div>
+                <input
+                    type='text'
+                    placeholder='Arama yapınız'
+                />
+            </div>
+        </div>
+    )
+}
+
+export default NavbarRight
