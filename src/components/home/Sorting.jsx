92b8bda48a00ba3b9f582e37e118c5c0fab08fa0
Emine Tekcan <eminetek68can@gmail.com>
2023-07-05 18:46:12 +0300

    Sorting component created

--- src/components/home/Sorting.jsx
+++ src/components/home/Sorting.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Sorting = () => {
   return (
-    <div>Sorting</div>
+    <div
+    className='bg-gray-100 my-3 p-3 flex items-center justify-end'
+    >
+      <select
+      className='bg-white outline-none py-3 px-5'
+      name="" id="">
+        <option disabled value="">SEÇİNİZ</option>
+        <option value="inc">ARTAN</option>
+        <option value="dec">AZALAN</option>
+      </select>
+    </div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,19 @@ import React from 'react'
 import CustomSlider from '../components/home/CustomSlider'
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Sorting from '../components/home/Sorting';
+import Category from '../components/home/Category';
+import Products from '../components/home/Products';
 
 const Home = () => {
     return (
         <div>
             <CustomSlider />
+            <Sorting />
+            <div className='flex'>
+                <Category />
+                <Products />
+            </div>
         </div>
     )
 }
