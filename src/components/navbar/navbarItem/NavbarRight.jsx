be33319cba6b21f08fb1b7a9ef20c7c52c19d02a
Emine Tekcan <eminetek68can@gmail.com>
2023-07-07 15:26:05 +0300

    Project completed

--- src/components/navbar/navbarItem/NavbarRight.jsx
+++ src/components/navbar/navbarItem/NavbarRight.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { BiSearch } from 'react-icons/bi'
 import { AiOutlineHeart } from 'react-icons/ai'
 import { SlBasket } from 'react-icons/sl'
@@ -10,7 +10,8 @@ const NavbarRight = () => {
 
     const dispatch = useDispatch();
     const { carts } = useSelector(state => state.carts)
-    const navigate=useNavigate();
+    const navigate = useNavigate();
+    const [search, setSearch] = useState("")
 
     useEffect(() => {
         const total = dispatch(getCartTotal())
@@ -20,6 +21,7 @@ const NavbarRight = () => {
         <div className='flex items-center gap-8'>
             <div className='flex items-center border p-3 rounded-full bg-gray-200'>
                 <input
+                    onChange={(e) => setSearch(e.target.value)}
                     type='text'
                     placeholder='Arama yapınız'
                     className='outline-none bg-gray-200'
@@ -27,7 +29,7 @@ const NavbarRight = () => {
                 <BiSearch className='cursor-pointer' size={28} />
             </div>
             <AiOutlineHeart className='cursor-pointer' size={28} />
-            <div onClick={()=>navigate("/cart")} className='cursor-pointer relative'>
+            <div onClick={() => navigate("/cart")} className='cursor-pointer relative'>
                 <div className='absolute -top-3 -right-3 bg-red-500 text-white rounded-full w-5 h-5 flex items-center justify-center'>{carts?.length}</div>
                 <SlBasket size={28} />
             </div>
